3891b3c3078d31029a0124a56d9e496a5df42bb4
Alessandro Marchesini <149405466+marchesinia187179@users.noreply.github.com>
2025-12-30 09:48:31 +0100

    Refactor form submission and add Fetch API

--- Lab04/esercizio_2/script.js
+++ Lab04/esercizio_2/script.js
@@ -1,7 +1,7 @@
 function getRadioCheckedValue(radioGroup) {
     for (option of radioGroup) {
         if (option.checked){
-            return option.values;
+            return option.value;
         }
     }
 }
@@ -21,8 +21,9 @@ function getSelectedValues(sel){
     return sel.options[sel.selectedIndex].value;
 }
 
+const registrationForm = document.getElementById('regForm');
 
-form.addEventListener('submit', function(event) {
+registrationForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent default form submission
   
     const username = document.getElementById('username').value;
@@ -40,17 +41,47 @@ form.addEventListener('submit', function(event) {
       interests: interests,
       country:  country
     };
-  
+
     // Use the Fetch API to make the POST request
+    fetch("https://httpbin.org/post", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(params)
+    })
+    .then(response => {
 
-    // Check if HTTP status is OK (200–299)
+        // Check if HTTP status is OK (200–299)
+        if (!response.ok) {
+            // Throwing makes the chain go to the catch()
+            throw new Error("Error response: " + response.status);
+        }
 
-    // Throwing makes the chain go to the catch()
-        
-    // Assuming result.data is a JSON-string that needs parsing
-      
+        return response.json();
 
-    // Handle the error
-      
+    })
+    .then(result => {
+
+        // Assuming result.data is a JSON-string that needs parsing
+        const data = JSON.parse(result.data);
+        const responseUsername = data.username;
+
+        /* Get the div for the response from the DOM
+         * and add the success string   */
+        document.getElementById("response").innerHTML = "Utente " + responseUsername + " creato!";
+        document.getElementById('response').style.color = 'green';
+
+    })
+    .catch(error => {   
+
+        /* Get the div for the response from the DOM
+         * and add the success string   */
+        document.getElementById("response").innerHTML = "Utente non creato!";
+        document.getElementById('response').style.color = 'red';
+
+        // Handle the error
+        console.error("Fetch error: ", error);
+
+    });
   });
-  
